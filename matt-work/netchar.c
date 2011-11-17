efa735647f7ea726e94e53a45680a7996bbe218e
Matthew Monaco <matthew.monaco@0x01b.net>
2011-11-16 19:07:18 -0700

    netchar: proper setup and creation of control device
    ...at least I think it's proper. This is a combination of my previous
    two trials. After going through various drivers and examples it seems to
    be the correct way to setup and create a device node.

--- matt-work/netchar.c
+++ matt-work/netchar.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/device.h>
 #include <linux/cdev.h>
 
 #define _MODULE_NAME     "netchar"
@@ -9,23 +10,28 @@
 #define _DEV_CONTROL     MKDEV(_MAJOR,0)
 #define _MAX_IMPORTS     16 /* very arbitrary */
 
+#define _PKE             KERN_ERR  _MODULE_NAME ": "
+#define _PKI             KERN_INFO _MODULE_NAME ": "
+
+struct device* netchar_device_ctrl;
+struct class*  netchar_class;
+struct cdev*   netchar_cdev;
+
 /*
  * CONTROL DEVICE
  */
 
-static struct cdev* cdev_control;
-
 static ssize_t netchar_read(struct file* fp, char* bufer,
-                           size_t length, loff_t* offset)
+                            size_t length, loff_t* offset)
 {
-	printk(KERN_INFO _MODULE_NAME ": control read");
+	printk(_PKI "control read");
 	return 0;
 }
 
 static ssize_t netchar_write(struct file* fp, const char* buffer,
                              size_t length, loff_t* offset)
 {
-	printk(KERN_INFO _MODULE_NAME ": control write");
+	printk(_PKE "control write");
 	return length;
 }
 
@@ -34,64 +40,6 @@ static struct file_operations fops_control = {
 	.read   = netchar_read,
 	.write  = netchar_write
 };
-
-static int netchar_init_control(void)
-{
-	int error;
-	
-	printk(KERN_INFO _MODULE_NAME ": init control dev\n");
-
-	/* register major,minor of control device*/
-
-	error = register_chrdev_region(_DEV_CONTROL, 1,_MODULE_NAME);
-
-	if (error < 0) {
-		printk(KERN_ERR _MODULE_NAME
-		       ": failed to register major number %i: %i\n",
-		       _MAJOR, -error);
-		goto err;
-	}
-
-	/* setup control device */
-
-	cdev_control = cdev_alloc();
-
-	if (cdev_control <= 0) {
-		printk(KERN_ERR _MODULE_NAME
-		       ": failed to allocate control cdev\n");
-		error = -ENOMEM;
-		goto err_unreg_chrdev;
-	}
-
-	cdev_init(cdev_control, &fops_control);
-	cdev_control->owner = THIS_MODULE;
-
-	/* register control device */
-
-	error = cdev_add(cdev_control, _DEV_CONTROL, 1);
-	
-	if (error < 0) {
-		printk(KERN_ERR _MODULE_NAME
-		       ": failed to add cdev: %i\n", -error);
-		goto err_del_cdev;
-	}
-
-	return 0;
-
-
-err_del_cdev:
-	cdev_del(cdev_control);
-err_unreg_chrdev:
-	unregister_chrdev_region(_DEV_CONTROL, 1);
-err:
-	return error;
-}
-
-static void netchar_exit_control(void)
-{
-	cdev_del(cdev_control);
-	unregister_chrdev_region(_DEV_CONTROL, 1);
-}
 
 /*
  * IMPORTED DEVICES
@@ -105,18 +53,81 @@ static void netchar_exit_control(void)
 static int __init netchar_init(void)
 {
 	int error;
-
-	printk(KERN_INFO _MODULE_NAME ": init\n");
 	
-	error = netchar_init_control();
+	printk(_PKI "initializing");
 
+	/* register major,minor of control device*/
+
+	error = register_chrdev_region(_DEV_CONTROL, 1,_MODULE_NAME);
+
+	if (error != 0) {
+		printk(_PKE "error registering control device: %i", -error);
+		goto init_err;
+	}
+	
+	/* setup cdev */
+
+	netchar_cdev = cdev_alloc();
+	error = PTR_ERR(netchar_cdev);
+
+	if (IS_ERR_VALUE(error)) {
+		printk(_PKE "error allocating cdev: %i", -error);
+		goto init_err_region;
+	}
+
+	netchar_cdev->owner = THIS_MODULE;
+	netchar_cdev->ops   = &fops_control;
+
+	/* add cdev */
+
+	error = cdev_add(netchar_cdev, _DEV_CONTROL, 1);
+
+	if (error != 0) {
+		printk(_PKE "error adding cdev: %i", -error);
+		goto init_err_cdev;
+	}
+
+	/* create device class (subsystem) */
+
+	netchar_class = class_create(THIS_MODULE, "netchar");
+	error = PTR_ERR(netchar_class);
+
+	if (IS_ERR_VALUE(error)) {
+		printk(_PKE "failed to create class: %i", -error);
+		goto init_err_cdev;
+	}
+
+	/* create device node in userspace */
+
+	netchar_device_ctrl = device_create(netchar_class, NULL, _DEV_CONTROL,
+	                                    NULL, "netchar_control");
+	error = PTR_ERR(netchar_device_ctrl);
+
+	if (IS_ERR_VALUE(error)) {
+		printk(_PKI "error creating device: %i", -error);
+		goto init_err_class;
+	}
+
+	return 0;
+
+	init_err_class:
+	class_destroy(netchar_class);
+	init_err_cdev:
+	cdev_del(netchar_cdev);
+	init_err_region:
+	unregister_chrdev_region(_DEV_CONTROL, 1);
+	init_err:
 	return error;
 }
 
 static void __exit netchar_exit(void)
 {
-	netchar_exit_control();
-	printk(KERN_INFO _MODULE_NAME ": exit\n");
+	device_destroy(netchar_class, _DEV_CONTROL);
+	class_destroy(netchar_class);
+	cdev_del(netchar_cdev);
+	unregister_chrdev_region(_DEV_CONTROL, 1);
+	
+	printk(_PKI "exit");
 }
 
 MODULE_LICENSE("GPL");
