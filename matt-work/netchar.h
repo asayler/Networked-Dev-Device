de18570464c5c2d576162a410064709b5f6df2e5
Matthew Monaco <matthew.monaco@0x01b.net>
2011-11-17 20:40:23 -0700

    netchar: add some stuff to header (very experimental)
    also, add the header as a make dependency to the ko

--- matt-work/netchar.h
+++ matt-work/netchar.h
@@ -3,4 +3,23 @@
 
 #define NETCHAR_NUM_DEVS    10
 
+enum { FOP_OPEN, FOP_READ, FOP_WRITE, FOP_RELEASE };
+
+struct netchar_open {
+	/* nothing */
+};
+
+struct netchar_read {
+	size_t size;
+	loff_t* offset;
+};
+
+struct netchar_msg {
+	int index;
+	int type;
+	union {
+		struct netchar_open      open;
+	} fop;
+};
+
 #endif /* _NETCHAR_H_ */
