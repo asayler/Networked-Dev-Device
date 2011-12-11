eba9f41f585d72ffa306d6d3d53e1974775c1905
Matthew Monaco <matthew.monaco@0x01b.net>
2011-12-10 23:55:04 -0700

    report: combine cdev-driver and fop-driver example

--- report/inc/cdev-driver.c
+++ report/inc/cdev-driver.c
@@ -6,3 +6,13 @@ static dev_t          nc_dev_t;
 static struct class*  nc_class;
 static struct cdev*   nc_cdev;
 static struct device* nc_device;
+
+static int netchar_open(struct inode* inp, struct file* fp);
+static ssize_t netchar_read(struct file* fp, char* buffer,
+size_t length, loff_t* offset);
+
+static struct file_operations nc_fops = {
+	/* ... */
+	.open  = nc_open,
+	.read  = nc_read,
+}
